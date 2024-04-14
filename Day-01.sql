127ab4eb35a608b62be5f5e41aa279ae0d1e110e
Mahmoudssanad <sanadmahmoud968@gmail.com>
2024-04-15 00:04:57 +0200

    Solution to the fifth problem

--- Day-01.sql
+++ Day-01.sql
@@ -22,3 +22,19 @@ Query the difference between the maximum and minimum populations in CITY.
 */
 --                   solution 
 select (max(population) - min(population)) from city;
+
+
+/* 
+                            question-04
+Query a count of the number of cities in CITY having a Population larger than 100000.
+*/
+--                   solution 
+select count(*) from city where population > 100000;
+
+
+/* 
+                            question-05
+Query the total population of all cities in CITY where District is California.
+*/
+--                   solution 
+select sum(population) from city where district in 'California';
