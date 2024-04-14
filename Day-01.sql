ba13894fc0195d46df039e82c6820f77a254cba9
Mahmoudssanad <sanadmahmoud968@gmail.com>
2024-04-14 18:05:17 +0200

    Solve frist problem

--- Day-01.sql
+++ Day-01.sql
@@ -1,1 +1,9 @@
 -- This is frist file in SQL problem solving
+
+/* 
+                            question
+Query the average population for all cities in CITY,
+rounded down to the nearest integer.
+*/
+--                   solution 
+select avg(population) from city;
