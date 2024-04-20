b31fa2bc017e4eb819b99442b9c7acaaef44db0f
Mahmoudssanad <sanadmahmoud968@gmail.com>
2024-04-21 00:53:21 +0200

    solution third problem

--- Day-02.sql
+++ Day-02.sql
@@ -12,3 +12,11 @@ Round your answer to 4 decimal places.*/
 --                   solution 
 select cast(round(long_w,4) as decimal(18,4)) from station where
     (lat_n in(select max(lat_n) from station where lat_n < 137.2345));
+
+
+    /* 
+                            question-03
+Query the smallest Northern Latitude (LAT_N) from STATION that is greater than 38.7780.
+Round your answer to 4 decimal places.*/
+--                   solution 
+select cast(round(min(lat_n),4) as decimal(18,4)) from station where lat_n > 38.7780;
