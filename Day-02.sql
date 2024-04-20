dcb88d11aa0d61ff9f99b6d13bc3e054d10f0eac
Mahmoudssanad <sanadmahmoud968@gmail.com>
2024-04-21 01:47:04 +0200

    solution fifth problem

--- Day-02.sql
+++ Day-02.sql
@@ -30,3 +30,20 @@ Round your answer to 4 decimal places.*/
 --                   solution 
 select cast(round(long_w,4) as decimal(18,4)) from station where (lat_n in 
 (select min(lat_n) from station where lat_n > 38.7780)) ;
+
+
+/* 
+                            question-05
+            consider P1(a,b) and P2(c,d)
+a happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
+b happens to equal the minimum value in Western Longitude (LONG_W in STATION).
+c happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
+d happens to equal the maximum value in Western Longitude (LONG_W in STATION).
+
+Query the Manhattan Distance between points P1 and P2
+and round it to a scale of 4 decimal places
+*/
+--                   solution 
+select cast(round (
+    ( max(lat_n) - min(lat_n) ) +  ( max(long_w) - min(long_w) )
+    , 4) as decimal (18, 4)) from station; 
