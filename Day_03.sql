d86f789bcb9f5e11533a85cc4417074f04663be8
Mahmoudssanad <sanadmahmoud968@gmail.com>
2024-04-23 03:12:17 +0200

    solved second problem

--- Day_03.sql
+++ Day_03.sql
@@ -12,3 +12,19 @@ select cast(round (
     sqrt(power((max(lat_n) - min(lat_n)),2) +
     power(( max(long_w) - min(long_w)),2))
     , 4) as decimal (18, 4)) from station;
+
+
+/* 
+                            question-02
+Given the CITY and COUNTRY tables, 
+query the names of all the continents (COUNTRY.Continent) and 
+their respective average city populations (CITY.Population) 
+rounded down to the nearest integer.
+
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+*/
+--                   solution 
+select COUNTRY.Continent, floor(avg(CITY.Population)) 
+from country, city
+where city.countrycode = country.code
+group by COUNTRY.Continent;
