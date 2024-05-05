cfdb289360112c4edc1387969d2c6bcc4a655363
Mahmoudssanad <sanadmahmoud968@gmail.com>
2024-05-05 02:02:34 +0200

    Solve Second Problem In File Day_04

--- Day_04.sql
+++ Day_04.sql
@@ -17,3 +17,25 @@ from wands w join Wands_Property wp
 on w.code = wp.code and is_evil = 0) as new_table
 where rn = 1
 order by power desc, age desc,coins_needed;
+
+
+/* 
+                            question-02
+Write a query identifying the type of each record in the TRIANGLES table 
+using its three side lengths. Output one of the following statements
+for each record in the table:
+
+Equilateral: It's a triangle with  sides of equal length.
+Isosceles: It's a triangle with  sides of equal length.
+Scalene: It's a triangle with  sides of differing lengths.
+Not A Triangle: The given values of A, B, and C don't form a triangle.
+*/
+--                   solution 
+select (case 
+            when (A = B and B = C) then "Equilateral"
+            when (A + B <= C OR B + C <= A OR A + C <= B) 
+            THEN "Not A Triangle"
+            when (A = B OR B = C OR A = C) THEN "Isosceles"
+            ELSE "Scalene"
+        END) AS Triangle_Type
+FROM TRIANGLES; 
