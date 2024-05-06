b4ecc2aadc02ef940293dd08be0ca7a59ac78e17
Mahmoudssanad <sanadmahmoud968@gmail.com>
2024-05-06 18:10:35 +0200

    Solve Problem Four In Day_04

--- Day_04.sql
+++ Day_04.sql
@@ -59,3 +59,29 @@ Write a solution to find the name, population, and area of the big countries.
 --                   solution 
 select name, population, area from world where
 area >= 3000000 or population >= 25000000;   
+
+
+
+/* 
+                            question-03
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| article_id    | int     |
+| author_id     | int     |
+| viewer_id     | int     |
+| view_date     | date    |
++---------------+---------+
+There is no primary key (column with unique values) for this table,
+the table may have duplicate rows.
+Each row of this table indicates that some viewer viewed an article
+(written by some author) on some date. 
+Note that equal author_id and viewer_id indicate the same person.
+
+Write a solution to find all the authors that viewed at least one of their own articles.
+Return the result table sorted by id in ascending order
+*/
+--                    solution 
+select distinct author_id as id from views 
+where author_id = viewer_id
+order by id asc;
